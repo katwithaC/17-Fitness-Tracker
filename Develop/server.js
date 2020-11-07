06ff5810ad9a0511b1d86bbbde2b805da1764223
katwithaC <catherinerosearredondo@gmail.com>
2020-11-06 23:27:59 -0600

    adding routes

---
+++ Develop/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+ 
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// routes
+app.use(require("./routes/routes.js"))
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/exercise.html'));
+});
+
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/stats.html'));
+});
+
+
+// Listen on port
+app.listen(PORT, () => {
+  console.log('App running on port ${PORT!}');
+});
